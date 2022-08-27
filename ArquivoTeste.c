a03ccdaf9c052996abbfe9ccfef1f69ee3372958
henriqueeduardosouza <henrique_eduardo_souza@hotmail.com>
2022-08-27 02:53:58 -0300

    adição função que imprime divisores

--- ArquivoTeste.c
+++ ArquivoTeste.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
+void print_divisors(int n){
+    int i;
+
+    for(i = 1; i <= n; i++)
+        if(n % i == 0)
+            printf("%d\n", i);
+}
+
 int main(){
-    printf("Hello World");
+    print_divisors(6);
     return 0;
 }
