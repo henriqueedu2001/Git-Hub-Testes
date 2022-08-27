7797cbb37b0c0624126431b34822824dd52862f6
henriqueeduardosouza <henrique_eduardo_souza@hotmail.com>
2022-08-27 03:01:55 -0300

    Comments Added

--- ArquivoTeste.c
+++ ArquivoTeste.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 
+/** this function prints all the positive divisors of n **/
 void print_divisors(int n){
     int i;
 
+    /** searches for divisors **/
     for(i = 1; i <= n; i++)
         if(n % i == 0)
             printf("%d\n", i);
